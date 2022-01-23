ba9f7503213d3b396174213d54cffc1224ff0aa9
Sheeva Kavousian <sheevakavous@gmail.com>
2022-01-23 18:54:49 +0000

    Decode HTML entities

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,16 @@
 import React from "react"
 import Answer from "./Answer"
+import he from "he"
 
 export default function Question(props) {
-    
+
     const answers = [...props.incorrect]
     answers.push(props.correct)
 
     const answerElements = answers.map((el, ind) => 
         <Answer 
             key={ind}
-            ans={el}
+            ans={he.decode(el)}
         />)
 
     return (
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,17 @@
 import React from "react"
-import { nativeTouchData } from "react-dom/cjs/react-dom-test-utils.production.min"
 import Question from "./Question"
+import he from "he"
 
 export default function Quiz(props) {
+
     const questionElements = props.quizQs.map((el, ind) => 
         <Question 
             key={ind}
-            question={el.question}
+            question={he.decode(el.question)}
             correct={el.correct_answer}
             incorrect={el.incorrect_answers}
-        />)
+        />
+    )
 
     return (
         <div>
